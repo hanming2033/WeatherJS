6d5f6d0b1fa60c51139b0367ebc75b40e9b080e9
Zheng Hanming <hanming2033@gmail.com>
2018-02-10 19:33:23 +0800

    added method to call ui.paint to paint to dom

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const weather = new Weather('Boston', 'MA');
+const ui = new UI();
 
 //Get weather on Dom load
 document.addEventListener('DOMContentLoaded', getWeather);
 
 function getWeather() {
   weather.getWeather()
-    .then(results => console.log(results))
+    .then(results => ui.paint(results))
     .catch(errrr => console.log(errrr))
 }
